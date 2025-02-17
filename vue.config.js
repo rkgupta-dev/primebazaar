d95b4f6563af684968d7e025b9b9f0356a1aba0c
rkgupta-dev <rohitkrgupta333@gmail.com>
2025-02-17 12:57:15 +0530

    deployed

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/primebazaar/" // This path is for production deployment
-      : "/", // This path is for local development
+  publicPath: "/",
 });
